cd2325e1915992e0b4164aae5d7ed67257a16589
SteveGan <stevegan17@icloud.com>
2019-05-24 10:11:49 +0800

    Basic file structure of our project.

---
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/HospitalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.neuedu.hospitalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalBackEndApplication.class, args);
+    }
+
+}
---
+++ hospital-front-end/src/components/common/Raven.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>{{info}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    this.$axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
