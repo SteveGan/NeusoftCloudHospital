d7456c8a01905aa3fb229097871e0b0ccb91c69e
SteveGan <stevegan17@icloud.com>
2019-05-27 15:33:02 +0800

    add common service interface: getPatientByCaseId getPatientByPatientName
    add a new po:
    Patient

--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceinterface/commonservice/PatientService.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceinterface/commonservice/PatientService.java
@@ -1,4 +1,24 @@
 package com.neuedu.hospitalbackend.service.serviceinterface.commonservice;
 
+import com.neuedu.hospitalbackend.model.po.Patient;
+import com.sun.tools.javac.util.List;
+
 public interface PatientService {
+
+    /**
+     * get one patient by case id
+     * @param caseId of the patient
+     * @return the patient
+     */
+    public Patient getPatientByCaseId(Integer caseId);
+
+
+    /**
+     * get the patient by patient names
+     * @param patientName of the patient
+     * @return list of patient with provided patient name.
+     */
+    public List<Patient> getPatientByPatientName(String patientName);
+
+
 }
