595489d14cf2c35da6af2f6dfdcd65a605f56dfa
Parachute <lyqwww123@163.com>
2019-05-27 17:54:13 +0800

    registrationservice 1.0 新加 Arrangement和User po

--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceinterface/registrationservice/RegistrationService.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceinterface/registrationservice/RegistrationService.java
@@ -1,6 +1,43 @@
 package com.neuedu.hospitalbackend.service.serviceinterface.registrationservice;
 
+import com.alibaba.fastjson.JSONObject;
+import com.neuedu.hospitalbackend.model.po.Arrangement;
+import com.neuedu.hospitalbackend.model.po.Patient;
+import com.neuedu.hospitalbackend.model.po.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface RegistrationService {
 
-    public void assignDoctor();
+    /**
+     * 检查患者是否已在本系统中
+     * @param IdCard 患者身份证号
+     * @return the patient
+     */
+    Patient queryPatient(String IdCard);
+
+    /**
+     * 通过挂号科室id，返回该科室在指定时间、指定号别仍有余号的看诊医生列表
+     * @param object 挂号科室id 指定时间 指定号别
+     * @return 有余号的看诊医生列表
+     */
+    List<User> listAvailableDoctors(JSONObject object);
+
+    /**
+     * 更新所选医生对应的余号数量
+     * @param user 需要更新的医生
+     */
+    void updateRemainingAppointment(User user);
+
+    /**
+     *
+     * @param object 看诊医生职称，挂号级别
+     * @return total_amount of a registration
+     */
+    int calculateAmount(JSONObject object);
+
+
+
 }
