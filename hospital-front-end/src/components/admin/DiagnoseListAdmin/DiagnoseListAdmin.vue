2a3a0023d0fdd9070c7f1e58ecbcce8e84b0ba64
SteveGan <stevegan17@icloud.com>
2019-05-29 22:03:53 +0800

    添加管理页面框架

---
+++ hospital-front-end/src/components/admin/DiagnoseListAdmin/DiagnoseListAdmin.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+<div class="">
+  DiagnoseList Admin
+</div>
+</template>
+
+<script>
+export default {
+  name: 'DiagnoseListAdmin'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ hospital-front-end/src/components/admin/ScheduleAdmin/ScheduleAdmin.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+<div class="">
+  Schedule Admin
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleAdmin'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
