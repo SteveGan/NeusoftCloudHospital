471426bf894e622b8ec46649b61ce40f15f9d9a1
Raven <raven.xu98@gmail.com>
2019-05-30 15:37:57 +0800

    add httprequest demo

--- hospital-back-end/src/test/java/com/neuedu/hospitalbackend/UserMapperTest.java
+++ hospital-back-end/src/test/java/com/neuedu/hospitalbackend/UserMapperTest.java
@@ -13,9 +13,4 @@ public class UserMapperTest {
         loginParam.setPassword("password");
         System.out.println(new OauthController().login(loginParam));
     }
-
-    @Test
-    public void testDemo() {
-        System.out.println(new DepartmentManagementController().selectDepartmentById("1"));
-    }
 }
