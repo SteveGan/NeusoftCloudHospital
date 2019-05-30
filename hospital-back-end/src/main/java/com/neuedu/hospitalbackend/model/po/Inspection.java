bf225c5a1d9aeecbfb4892474e8e396d065c34f0
oamyjin <13684833271@163.com>
2019-05-30 13:25:54 +0800

    获取所有user1.0

--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/model/po/Inspection.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/model/po/Inspection.java
@@ -1,37 +1,135 @@
 package com.neuedu.hospitalbackend.model.po;
 
-import com.alibaba.fastjson.annotation.JSONField;
-
-import java.sql.Date;
+import java.util.Date;
 
 public class Inspection {
-
     private Integer id;
 
     private Integer caseId;
 
     private Integer projectId;
 
-    private Integer creatorId;
+    private Byte status;
 
-    private Integer inspectorId;
-
-    private Boolean status;
+    private Date gmtModified;
 
     private String goal;
 
-    private String requirement;
-
     private String resultImage;
 
-    private String resultPicture;
-
     private String resultDescription;
+
+    private Integer creatorRoleId;
+
+    private Integer inspectorRoleId;
 
     private String advice;
 
     private Date gmtCreate;
 
-    private Date gmtModified;
+    private String requirement;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCaseId() {
+        return caseId;
+    }
+
+    public void setCaseId(Integer caseId) {
+        this.caseId = caseId;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal == null ? null : goal.trim();
+    }
+
+    public String getResultImage() {
+        return resultImage;
+    }
+
+    public void setResultImage(String resultImage) {
+        this.resultImage = resultImage == null ? null : resultImage.trim();
+    }
+
+    public String getResultDescription() {
+        return resultDescription;
+    }
+
+    public void setResultDescription(String resultDescription) {
+        this.resultDescription = resultDescription == null ? null : resultDescription.trim();
+    }
+
+    public Integer getCreatorRoleId() {
+        return creatorRoleId;
+    }
+
+    public void setCreatorRoleId(Integer creatorRoleId) {
+        this.creatorRoleId = creatorRoleId;
+    }
+
+    public Integer getInspectorRoleId() {
+        return inspectorRoleId;
+    }
+
+    public void setInspectorRoleId(Integer inspectorRoleId) {
+        this.inspectorRoleId = inspectorRoleId;
+    }
+
+    public String getAdvice() {
+        return advice;
+    }
+
+    public void setAdvice(String advice) {
+        this.advice = advice == null ? null : advice.trim();
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public String getRequirement() {
+        return requirement;
+    }
+
+    public void setRequirement(String requirement) {
+        this.requirement = requirement == null ? null : requirement.trim();
+    }
 }
--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceimplementation/basicinfomanagementservice/OauthServiceImpl.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceimplementation/basicinfomanagementservice/OauthServiceImpl.java
@@ -1,15 +1,18 @@
 package com.neuedu.hospitalbackend.service.serviceimplementation.basicinfomanagementservice;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.neuedu.hospitalbackend.model.dto.LoginParam;
 import com.neuedu.hospitalbackend.model.dao.RoleMapper;
 import com.neuedu.hospitalbackend.model.dao.UserMapper;
+import com.neuedu.hospitalbackend.model.po.Patient;
 import com.neuedu.hospitalbackend.model.po.Role;
 import com.neuedu.hospitalbackend.model.po.User;
 import com.neuedu.hospitalbackend.service.serviceinterface.basicinfomanagementservice.OauthService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -40,6 +43,36 @@ public class OauthServiceImpl implements OauthService {
             result.put("success", false);
         }
         return result;
+    }
 
+    /**
+     * 所有用户以及角色信息
+     * @return jsonArray:[
+     *                      user:{
+     *                              id...
+     *                              role:[]
+     *                       },
+     *                     ]
+     */
+    @Override
+    public JSONArray listAllUsersAndRoles(){
+        JSONArray returnArray = new JSONArray();
+
+        List<User> users = userMapper.listAllUsersAndRoles();
+
+        for(User user : users){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("userId", user.getId());
+            jsonObject.put("userName", user.getName());
+            jsonObject.put("userAvatar", user.getAvatar());
+            jsonObject.put("userCreateTime",user.getGmtCreate());
+            List<Role> roles = user.getRoles();
+            for(Role role: roles){
+                System.out.println("rrrrrrrrrrrrrrr"+role.getId());
+            }
+            jsonObject.put("roles",user.getRoles());
+            returnArray.add(jsonObject);
+        }
+        return returnArray;
     }
 }
