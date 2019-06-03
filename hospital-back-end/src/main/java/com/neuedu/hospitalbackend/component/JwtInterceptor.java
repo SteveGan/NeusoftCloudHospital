c4bd151a5e0a0872bf46d317092d1408e3d02dcc
oamyjin <13684833271@163.com>
2019-06-03 18:12:54 +0800

    查询未登记结果的项目1.0

--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/component/JwtInterceptor.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/component/JwtInterceptor.java
@@ -13,21 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class JwtInterceptor extends HandlerInterceptorAdapter {
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer:")) {
-            throw new UserLoginException("用户未登录");
-
-        }
-        //取得token
-        String token = authHeader.substring(7);
-
-        //验证token
-        Claims claims = JwtUtil.checkToken(token);
-        request.setAttribute("username", claims.getSubject());
-        return true;
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//
+//        String authHeader = request.getHeader("Authorization");
+//        if (authHeader == null || !authHeader.startsWith("Bearer:")) {
+//            throw new UserLoginException("用户未登录");
+//
+//        }
+//        //取得token
+//        String token = authHeader.substring(7);
+//
+//        //验证token
+//        Claims claims = JwtUtil.checkToken(token);
+//        request.setAttribute("username", claims.getSubject());
+//        return true;
+//    }
 
 }
