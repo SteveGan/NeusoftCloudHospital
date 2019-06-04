3c941000ab7ec486d79f625f41d8122f9c33dde3
SteveGan <stevegan17@icloud.com>
2019-06-04 10:42:50 +0800

    门诊医生所有模块prototype完毕

--- hospital-front-end/src/pages/OutPatientDoctor/OutPatientDoctor.vue
+++ hospital-front-end/src/pages/OutPatientDoctor/OutPatientDoctor.vue
@@ -256,7 +256,7 @@
             <!-- 左侧操作区 -->
             <div class="service-main-container">
               <!-- 工具栏 -->
-              <el-card :body-style="{padding:'0px'}" style="margin-bottom: 5px">
+              <el-card :body-style="{padding:'0px'}" style="margin-bottom: 5px;">
                 <el-button type="text" icon="el-icon-refresh-right" round>清屏</el-button>
                 <el-button type="text" icon="el-icon-folder-checked" round>暂存</el-button>
                 <el-button type="text" icon="el-icon-printer" round>打印</el-button>
@@ -318,11 +318,427 @@
             </div>
           </div>
         </el-tab-pane>
-        <el-tab-pane label="成药处方">角色管理</el-tab-pane>
-        <el-tab-pane label="草药处方">定时任务补偿</el-tab-pane>
-        <el-tab-pane label="检查申请">定时任务补偿</el-tab-pane>
-        <el-tab-pane label="处置单">定时任务补偿</el-tab-pane>
-        <el-tab-pane label="患者账单">定时任务补偿</el-tab-pane>
+        <el-tab-pane label="检查申请">
+          <div class="outpatient-service-container">
+            <!-- 左侧操作区 -->
+            <div class="service-main-container">
+              <!-- 工具栏 -->
+              <el-card :body-style="{padding:'0px'}" style="margin-bottom: 5px;">
+                <el-button type="text" icon="el-icon-refresh-right" round>清屏</el-button>
+                <el-button type="text" icon="el-icon-folder-checked" round>暂存</el-button>
+                <el-button type="text" icon="el-icon-printer" round>打印</el-button>
+              </el-card>
+              <!-- 检验申请的操作栏以及表格, 做成一个卡片 -->
+              <el-card>
+                <!-- 操作栏 -->
+                <div slot="header" class="clearfix">
+                    <span>检验申请单</span>
+                    <el-button style="float:right" type="text" icon="el-icon-document-add" round>存为模版</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-s-check">开立</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-delete-solid">作废</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-remove">删除项目</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-circle-plus">新增项目</el-button>
+                </div>
+                <!-- 项目列表 -->
+                <div class="">
+                  <el-table
+                    :data="tableData"
+                    style="width: 100%">
+                    <el-table-column
+                      type="selection"
+                      width="55">
+                    </el-table-column>
+                    <el-table-column
+                      label="申请名称">
+                    </el-table-column>
+                    <el-table-column
+                      label="项目名称">
+                    </el-table-column
+                      label="执行科室">
+                    <el-table-column
+                      label="执行状态">
+                    </el-table-column>
+                    <el-table-column
+                      label="检查结果">
+                    </el-table-column>
+                  </el-table>
+                </div>
+              </el-card>
+            </div>
+            <!-- 右侧模版区域 -->
+            <div class="service-side-container">
+              <!-- 导航栏(也就是一个标签页) -->
+              <el-tabs type="border-card" class="template-tabs">
+                <!-- 门诊首页tab-->
+                <el-tab-pane label="病历模版">
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                </el-tab-pane>
+                <el-tab-pane label="常用诊断"></el-tab-pane>
+                <el-tab-pane label="历史病历"></el-tab-pane>
+              </el-tabs>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="成药处方">
+          <div class="recipe-service-container">
+            <!-- 主操作区 -->
+            <div>
+              <!-- 工具栏 -->
+              <el-card :body-style="{padding:'0px'}" style="margin-bottom: 5px;">
+                <el-button type="text" icon="el-icon-refresh-right" round>清屏</el-button>
+                <el-button type="text" icon="el-icon-folder-checked" round>暂存</el-button>
+                <el-button type="text" icon="el-icon-printer" round>打印</el-button>
+              </el-card>
+              <!-- 检验申请的操作栏以及表格, 做成一个卡片 -->
+              <el-card>
+                <!-- 操作栏 -->
+                <div slot="header" class="clearfix">
+                    <span>处方 1</span>
+                    <el-button style="float:right" type="text" icon="el-icon-s-check">开立</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-delete-solid">删除（作废）</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-remove">增药</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-circle-plus">删药</el-button>
+                </div>
+                <!-- 项目列表 -->
+                <div class="">
+                  <el-table
+                    :data="tableData"
+                    style="width: 100%">
+                    <el-table-column
+                      type="selection"
+                      width="55">
+                    </el-table-column>
+                    <el-table-column
+                      label="药品名称">
+                    </el-table-column>
+                    <el-table-column
+                      label="规格">
+                    </el-table-column
+                      label="单价">
+                    <el-table-column
+                      label="用法">
+                    </el-table-column>
+                    <el-table-column
+                      label="用量">
+                    </el-table-column>
+                    <el-table-column
+                      label="单位">
+                    </el-table-column>
+                    <el-table-column
+                      label="频次">
+                    </el-table-column>
+                    <el-table-column
+                      label="天数">
+                    </el-table-column>
+                    <el-table-column
+                      label="次数">
+                    </el-table-column>
+                    <el-table-column
+                      label="数量">
+                    </el-table-column>
+                    <el-table-column
+                      label="单位">
+                    </el-table-column>
+                    <el-table-column
+                      label="皮试">
+                    </el-table-column>
+                    <el-table-column
+                      label="皮试结果">
+                    </el-table-column>
+                    <el-table-column
+                      label="自购">
+                    </el-table-column>
+                    <el-table-column
+                      label="用药嘱托">
+                    </el-table-column>
+                  </el-table>
+                </div>
+              </el-card>
+              <!-- 添加处方按钮 -->
+              <div class="add-recipe-button">
+                <el-button type="text" icon="el-icon-plus" round>新增处方</el-button>
+              </div>
+            </div>
+            <!-- 底部模版区域 -->
+            <div>
+              <!-- 导航栏(也就是一个标签页) -->
+              <el-tabs type="border-card" class="template-tabs">
+                <!-- 门诊首页tab-->
+                <el-tab-pane label="处方模版">
+                  <div class="recipe-template">
+                    <!-- 左侧browser区 -->
+                    <div class="recipe-browser">
+                      <el-card>
+                      </el-card>
+                    </div>
+                    <!-- 右侧处方内容展示区-->
+                    <div class="recipe-detail">
+                      <el-card>
+                      </el-card>
+                    </div>
+                  </div>
+
+
+                </el-tab-pane>
+                <el-tab-pane label="常用药品"></el-tab-pane>
+                <el-tab-pane label="建议方案"></el-tab-pane>
+                <el-tab-pane label="历史处方"></el-tab-pane>
+              </el-tabs>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="草药处方">
+          <div class="recipe-service-container">
+            <!-- 主操作区 -->
+            <div>
+              <!-- 工具栏 -->
+              <el-card :body-style="{padding:'0px'}" style="margin-bottom: 5px;">
+                <el-button type="text" icon="el-icon-refresh-right" round>清屏</el-button>
+                <el-button type="text" icon="el-icon-folder-checked" round>暂存</el-button>
+                <el-button type="text" icon="el-icon-printer" round>打印</el-button>
+              </el-card>
+              <!-- 检验申请的操作栏以及表格, 做成一个卡片 -->
+              <el-card>
+                <!-- 操作栏 -->
+                <div slot="header" class="clearfix">
+                    <span>处方 1</span>
+                    <el-button style="float:right" type="text" icon="el-icon-s-check">开立</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-delete-solid">删除（作废）</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-remove">增药</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-circle-plus">删药</el-button>
+                </div>
+                <!-- 项目列表 -->
+                <div class="">
+                  <el-table
+                    :data="tableData"
+                    style="width: 100%">
+                    <el-table-column
+                      type="selection"
+                      width="55">
+                    </el-table-column>
+                    <el-table-column
+                      label="药品名称">
+                    </el-table-column>
+                    <el-table-column
+                      label="规格">
+                    </el-table-column
+                      label="单价">
+                    <el-table-column
+                      label="用法">
+                    </el-table-column>
+                    <el-table-column
+                      label="用量">
+                    </el-table-column>
+                    <el-table-column
+                      label="单位">
+                    </el-table-column>
+                    <el-table-column
+                      label="频次">
+                    </el-table-column>
+                    <el-table-column
+                      label="天数">
+                    </el-table-column>
+                    <el-table-column
+                      label="次数">
+                    </el-table-column>
+                    <el-table-column
+                      label="数量">
+                    </el-table-column>
+                    <el-table-column
+                      label="单位">
+                    </el-table-column>
+                    <el-table-column
+                      label="皮试">
+                    </el-table-column>
+                    <el-table-column
+                      label="皮试结果">
+                    </el-table-column>
+                    <el-table-column
+                      label="自购">
+                    </el-table-column>
+                    <el-table-column
+                      label="用药嘱托">
+                    </el-table-column>
+                  </el-table>
+                </div>
+              </el-card>
+              <!-- 添加处方按钮 -->
+              <div class="add-recipe-button">
+                <el-button type="text" icon="el-icon-plus" round>新增处方</el-button>
+              </div>
+            </div>
+            <!-- 底部模版区域 -->
+            <div>
+              <!-- 导航栏(也就是一个标签页) -->
+              <el-tabs type="border-card" class="template-tabs">
+                <!-- 门诊首页tab-->
+                <el-tab-pane label="处方模版">
+                  <div class="recipe-template">
+                    <!-- 左侧browser区 -->
+                    <div class="recipe-browser">
+                      <el-card>
+                      </el-card>
+                    </div>
+                    <!-- 右侧处方内容展示区-->
+                    <div class="recipe-detail">
+                      <el-card>
+                      </el-card>
+                    </div>
+                  </div>
+
+
+                </el-tab-pane>
+                <el-tab-pane label="常用药品"></el-tab-pane>
+                <el-tab-pane label="建议方案"></el-tab-pane>
+                <el-tab-pane label="历史处方"></el-tab-pane>
+              </el-tabs>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="处置单">
+          <div class="outpatient-service-container">
+            <!-- 左侧操作区 -->
+            <div class="service-main-container">
+              <!-- 工具栏 -->
+              <el-card :body-style="{padding:'0px'}" style="margin-bottom: 5px;">
+                <el-button type="text" icon="el-icon-refresh-right" round>清屏</el-button>
+                <el-button type="text" icon="el-icon-folder-checked" round>暂存</el-button>
+                <el-button type="text" icon="el-icon-printer" round>打印</el-button>
+              </el-card>
+              <!-- 处置申请的操作栏以及表格, 做成一个卡片 -->
+              <el-card>
+                <!-- 操作栏 -->
+                <div slot="header" class="clearfix">
+                    <span>申请单列表</span>
+                    <el-button style="float:right" type="text" icon="el-icon-document-add" round>存为模版</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-s-check">开立</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-delete-solid">作废</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-remove">删除项目</el-button>
+                    <el-button style="float:right" type="text" icon="el-icon-circle-plus">新增项目</el-button>
+                </div>
+                <!-- 项目列表 -->
+                <div class="">
+                  <el-table
+                    :data="tableData"
+                    style="width: 100%">
+                    <el-table-column
+                      type="selection"
+                      width="55">
+                    </el-table-column>
+                    <el-table-column
+                      label="申请单号">
+                    </el-table-column>
+                    <el-table-column
+                      label="申请时间">
+                    </el-table-column
+                      label="状态">
+                    <el-table-column
+                      label="申请人">
+                    </el-table-column>
+                    <el-table-column
+                      label="收费状态">
+                    </el-table-column>
+                    <el-table-column
+                      label="执行状态">
+                    </el-table-column>
+                    <el-table-column
+                      label="金额">
+                    </el-table-column>
+                  </el-table>
+                </div>
+              </el-card>
+              <!-- 所有可选处置项目 -->
+              <el-card>
+                <!-- 操作栏 -->
+                <div slot="header" class="clearfix">
+                    <span>项目列表</span>
+                </div>
+                <!-- 项目列表 -->
+                <div class="">
+                  <el-table
+                    :data="tableData"
+                    style="width: 100%">
+                    <el-table-column
+                      type="selection"
+                      width="55">
+                    </el-table-column>
+                    <el-table-column
+                      label="类别">
+                    </el-table-column>
+                    <el-table-column
+                      label="通用名">
+                    </el-table-column
+                      label="剩余库存">
+                    <el-table-column
+                      label="执行科室">
+                    </el-table-column>
+                  </el-table>
+                </div>
+              </el-card>
+            </div>
+            <!-- 右侧模版区域 -->
+            <div class="service-side-container">
+              <!-- 导航栏(也就是一个标签页) -->
+              <el-tabs type="border-card" class="template-tabs">
+                <!-- 门诊首页tab-->
+                <el-tab-pane label="常用项目">
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                  <p>haha</p>
+                </el-tab-pane>
+                <el-tab-pane label="处置模版"></el-tab-pane>
+              </el-tabs>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="患者账单">
+          <el-card>
+            <!-- 操作栏 -->
+            <div slot="header" class="clearfix">
+                <span>申请单列表</span>
+            </div>
+            <!-- 项目列表 -->
+            <div class="">
+              <el-table
+                :data="tableData"
+                style="width: 100%">
+                <el-table-column
+                  type="selection"
+                  width="55">
+                </el-table-column>
+                <el-table-column
+                  label="名称">
+                </el-table-column>
+                <el-table-column
+                  label="规格">
+                </el-table-column
+                  label="数量">
+                <el-table-column
+                  label="付数">
+                </el-table-column>
+                <el-table-column
+                  label="单位">
+                </el-table-column>
+                <el-table-column
+                  label="金额">
+                </el-table-column>
+                <el-table-column
+                  label="收费状态">
+                </el-table-column>
+              </el-table>
+            </div>
+          </el-card>
+        </el-tab-pane>
       </el-tabs>
     </el-main>
   </el-container>
@@ -404,6 +820,28 @@ export default {
   margin-top: 38px;
 }
 
+.recipe-service-container{
+  display:flex;
+  flex-direction: column;
+}
+
+.recipe-template{
+  display: grid;
+  grid-template-columns: 40% 60%;
+}
+
+.recipe-browser{
+  grid-column: 1/2;
+  padding: 2px;
+}
+
+.recipe-detail{
+  grid-column: 2;
+  padding: 2px;
+}
+
+
+
 .template-tabs{
   position: -webkit-sticky;
   position: sticky;
@@ -434,6 +872,15 @@ export default {
   font-size: 15px;
   padding: 0px 3px;
   color: white;
+}
+
+.service-main-container .el-card{
+  margin-right: 5px;
+}
+
+.add-recipe-button{
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 
 
