a709e0b04359fb0da5fe1b231ff2534d8e0de372
Raven <raven.xu98@gmail.com>
2019-06-10 13:51:33 +0800

    add techproject po, and constant controller

---
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/model/po/TechProject.java
@@ -0,0 +1,85 @@
+package com.neuedu.hospitalbackend.model.po;
+
+import java.math.BigDecimal;
+
+public class TechProject {
+    private Integer id;
+
+    private String itemId;
+
+    private String itemName;
+
+    private Short projectId;
+
+    private String projectName;
+
+    private String unit;
+
+    private BigDecimal price;
+
+    private Integer departmentId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId == null ? null : itemId.trim();
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName == null ? null : itemName.trim();
+    }
+
+    public Short getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Short projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName == null ? null : projectName.trim();
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit == null ? null : unit.trim();
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+}
