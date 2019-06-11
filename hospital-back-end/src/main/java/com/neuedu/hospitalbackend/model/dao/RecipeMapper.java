d217794e99e28ed780489604cf998c3c186c7cbc
oamyjin <13684833271@163.com>
2019-06-11 17:25:37 +0800

    list处方+申请项目小改

--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/model/dao/RecipeMapper.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/model/dao/RecipeMapper.java
@@ -10,12 +10,14 @@ import java.util.List;
 @Component
 public interface RecipeMapper {
 
-    int updateRemainAmount(@Param("collectionId") Integer collectionId, @Param("projectId") Integer projectId, @Param("returnAmount") Short returnAmount);
+    int updateRemainAmount(@Param("collectionId") Integer collectionId, @Param("projectId") Integer projectId,
+                           @Param("returnAmount") Short returnAmount);
 
     //根据患者的病历号，列出处方信息列表
     List<HashMap> listRecipeByRegistrationId(Integer registrationId);
 
-    int updateStatus(@Param("collectionId") Integer collectionId, @Param("medicineId") Integer medicineId, @Param("status") Byte status);
+    int updateStatus(@Param("collectionId") Integer collectionId, @Param("medicineId") Integer medicineId,
+                     @Param("status") Byte status);
 
     int updateStatusAndDeliverId(@Param("collectionId") Integer collectionId, @Param("medicineId") Integer medicineId,
                                  @Param("status") Byte status, @Param("deliverId") Integer deliverId);
@@ -23,8 +25,10 @@ public interface RecipeMapper {
     int updateInventory(@Param("medicineId") Integer medicineId, @Param("amount") Short amount);
 
     //列出指定日期或指定病历号的处方信息
-    List<HashMap> listRecipesByRegistrationIdOrDate(@Param("caseId") Integer caseId, @Param("chargeDateBeginStr") String chargeDateBeginStr, @Param("chargeDateEndStr") String chargeDateEndStr);
+    List<HashMap> listRecipesByRegistrationIdOrDate(@Param("caseId") Integer caseId,
+                                                    @Param("chargeDateBeginStr") String chargeDateBeginStr,
+                                                    @Param("chargeDateEndStr") String chargeDateEndStr);
 
-
+    List<HashMap> listRecipeInfoByRegistrationId(Integer caseId);
 
 }
--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceimplementation/doctorstationservice/RecipeManagementServiceImpl.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceimplementation/doctorstationservice/RecipeManagementServiceImpl.java
@@ -1,15 +1,19 @@
 package com.neuedu.hospitalbackend.service.serviceimplementation.doctorstationservice;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.neuedu.hospitalbackend.model.dao.RecipeMapper;
 import com.neuedu.hospitalbackend.service.serviceinterface.doctorstationservice.RecipeManagementService;
 import com.neuedu.hospitalbackend.util.CommonResult;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class RecipeManagementServiceImpl implements RecipeManagementService {
@@ -20,18 +24,57 @@ public class RecipeManagementServiceImpl implements RecipeManagementService {
     @Override
     public CommonResult listCurrentCaseRecipes(Integer caseId) {
         JSONObject returnJson = new JSONObject();
-        List<HashMap> recipes =  recipeMapper.listRecipeByRegistrationId(caseId);
-        if(recipes == null)
+
+        List<HashMap> recipeLogs = recipeMapper.listRecipeInfoByRegistrationId(caseId);
+        //处方类型：0该患者无处方，1中草药处方，2其他处方（成药）
+        if(recipeLogs == null)
             returnJson.put("type", 0);
-        else{
-            String type = (String)recipes.get(0).get("medicineType");
-            if(type == "中草药")
-                returnJson.put("type", 1);
+        if((Integer)recipeLogs.get(0).get("medicineType") == 1)
+            returnJson.put("type", 1);//中草药
+        else
+            returnJson.put("type", 2);//成药
+
+        //转换格式
+        HashMap<Integer, List<HashMap>> recipes = new HashMap<>();
+        for(HashMap recipeLog : recipeLogs){
+            Long recipeIdLong = (Long)recipeLog.get("id");
+            Integer recipeId = new Integer(String.valueOf(recipeIdLong));
+            List<HashMap> medicines;
+            if(!recipes.containsKey(recipeId))
+                medicines = new ArrayList<>();
             else
-                returnJson.put("type", 2);
+                medicines = recipes.get(recipeId);
+            medicines.add(medicineInfo(recipeLog));
+            recipes.put(recipeId, medicines);
         }
-        returnJson.put("recipes", recipes);
-        return null;
+        JSONArray jsonArray = new JSONArray();
+        for(Map.Entry<Integer, List<HashMap>> entry: recipes.entrySet()) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("recipeId", entry.getKey());
+            jsonObject.put("medicines", entry.getValue());
+            jsonArray.add(jsonObject);
+        }
+        returnJson.put("recipes", jsonArray);
+        return CommonResult.success(returnJson);
+    }
+
+    /**
+     * 格式转换
+     * @param recipeLog
+     * @return
+     */
+    public HashMap medicineInfo(HashMap recipeLog){
+        HashMap medicine = new HashMap();
+        medicine.put("medicineId", recipeLog.get("medicineId"));
+        medicine.put("medicineName", recipeLog.get("medicineName"));
+        medicine.put("amount", recipeLog.get("amount"));
+        medicine.put("dosage", recipeLog.get("dosage"));
+        medicine.put("frequency", recipeLog.get("frequency"));
+        medicine.put("medicineUnit", recipeLog.get("medicineUnit"));
+        medicine.put("medicineType", recipeLog.get("medicineType"));
+        medicine.put("medicineFormulation", recipeLog.get("medicineFormulation"));
+        medicine.put("medicineSpecification", recipeLog.get("medicineSpecification"));
+        return medicine;
     }
 
 
