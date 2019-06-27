5386d63db4ce24e86272d332666e623d94c96c6b
oamyjin <13684833271@163.com>
2019-06-27 15:05:43 +0800

    少了个逗号，，，

--- hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceimplementation/doctorstationservice/RecipeManagementServiceImpl.java
+++ hospital-back-end/src/main/java/com/neuedu/hospitalbackend/service/serviceimplementation/doctorstationservice/RecipeManagementServiceImpl.java
@@ -205,6 +205,7 @@ public class RecipeManagementServiceImpl implements RecipeManagementService {
             recipe.setMedicineUnitPrice((BigDecimal) hashMap.get("unitPrice"));
             recipeParam.setMedicineUnitPrice((BigDecimal) hashMap.get("unitPrice"));
             //插入数据库
+            //===============================================
             count = recipeMapper.insertSelective(recipe);
             if(count <= 0)
                 return CommonResult.fail(ResultCode.E_802);
