e08db76c52bfc6bc3fe724921a7af92511bcb585
Raven <raven.xu98@gmail.com>
2019-06-28 13:11:33 +0800

    小程序更新

--- hospital-wxapp/dist_vant/switch/index.js
+++ hospital-wxapp/dist_vant/switch/index.js
@@ -1,49 +1,43 @@
 import { VantComponent } from '../common/component';
 VantComponent({
-  field: true,
-  classes: ['node-class'],
-  props: {
-    checked: Boolean,
-    loading: Boolean,
-    disabled: Boolean,
-    activeColor: String,
-    inactiveColor: String,
-    size: {
-      type: String,
-      value: '30px'
-    }
-  },
-  watch: {
-    checked: function checked(value) {
-      this.setData({
-        value: value
-      });
-    }
-  },
-  computed: {
-    classes: function classes() {
-      return this.classNames('custom-class', 'van-switch', {
-        'van-switch--on': this.data.checked,
-        'van-switch--disabled': this.data.disabled
-      });
+    field: true,
+    classes: ['node-class'],
+    props: {
+        checked: null,
+        loading: Boolean,
+        disabled: Boolean,
+        activeColor: String,
+        inactiveColor: String,
+        size: {
+            type: String,
+            value: '30px'
+        },
+        activeValue: {
+            type: null,
+            value: true
+        },
+        inactiveValue: {
+            type: null,
+            value: false
+        }
     },
-    style: function style() {
-      var backgroundColor = this.data.checked ? this.data.activeColor : this.data.inactiveColor;
-      return "font-size: " + this.data.size + "; " + (backgroundColor ? "background-color: " + backgroundColor : '');
+    watch: {
+        checked(value) {
+            this.set({ value });
+        }
+    },
+    created() {
+        this.set({ value: this.data.checked });
+    },
+    methods: {
+        onClick() {
+            const { activeValue, inactiveValue } = this.data;
+            if (!this.data.disabled && !this.data.loading) {
+                const checked = this.data.checked === activeValue;
+                const value = checked ? inactiveValue : activeValue;
+                this.$emit('input', value);
+                this.$emit('change', value);
+            }
+        }
     }
-  },
-  created: function created() {
-    this.setData({
-      value: this.data.checked
-    });
-  },
-  methods: {
-    onClick: function onClick() {
-      if (!this.data.disabled && !this.data.loading) {
-        var checked = !this.data.checked;
-        this.$emit('input', checked);
-        this.$emit('change', checked);
-      }
-    }
-  }
 });
